a9e8905122b60b461994101f40780c4998e09c58
Rama Ayodhya <ramaaydhya@gmail.com>
2024-07-22 21:10:14 +0700

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -10,6 +10,50 @@ import (
 	"os"
 )
 
+func getPath(buff *[]byte) []byte {
+	var after []byte
+	if isGet(buff) {
+		after, _ = bytes.CutPrefix(*buff, []byte("GET "))
+	}
+	var pathEnd = bytes.IndexByte(after, byte(' '))
+	var path = make([]byte, pathEnd)
+	copy(path, after[:pathEnd])
+	return path
+}
+
+func isGet(buff *[]byte) bool {
+	return bytes.HasPrefix(*buff, []byte("GET"))
+}
+
+func respondGet(conn *net.Conn, status int, respBody *[]byte) (err error) {
+	var bodyLen = len(*respBody)
+	var conLen = []byte(strconv.Itoa(bodyLen))
+	var respLen = 0
+
+	var statusLine = []byte("HTTP/1.1 ")
+	switch status {
+	case 200:
+		statusLine = append(statusLine, []byte("200 OK\r\n")...)
+	case 404:
+		statusLine = append(statusLine, []byte("404 Not Found\r\n")...)
+	}
+	respLen += len(statusLine)
+
+	var respHeader = []byte("Content-Type: text/plain\r\nContent-Length: ")
+	respHeader = append(respHeader, conLen...)
+	respHeader = append(respHeader, []byte("\r\n\r\n")...)
+	respLen += len(respHeader)
+
+	respLen += bodyLen
+
+	var resp = make([]byte, 0, respLen)
+	resp = append(resp, statusLine...)
+	resp = append(resp, respHeader...)
+	resp = append(resp, (*respBody)...)
+	_, err = (*conn).Write(resp)
+	return
+}
+
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
@@ -23,6 +67,7 @@ func main() {
 	}
 
 	conn, err := listener.Accept()
+
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
@@ -32,34 +77,29 @@ func main() {
 
 	var req = make([]byte, 1024)
 	conn.Read(req)
-
-	if bytes.HasPrefix(req, []byte("GET / HTTP/1.1")) {
+	var path = getPath(&req)
+	if isGet(&req) && bytes.Equal(path, []byte("/")) {
 		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
 		return
 	}
-
-	var getEchoPrefix = []byte("GET /echo/")
-	if !bytes.HasPrefix(req, getEchoPrefix) {
-		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+	if isGet(&req) && bytes.Equal(path, []byte("/user-agent")) {
+		var idx = bytes.Index(req, []byte("User-Agent: "))
+		var uaBegin = idx + len("User-Agent: ")
+		var uaEnd = uaBegin
+		for req[uaEnd] != byte('\r') {
+			uaEnd++
+		}
+		var userAgent = make([]byte, uaEnd-uaBegin)
+		copy(userAgent, req[uaBegin:uaEnd])
+		respondGet(&conn, 200, &userAgent)
 		return
 	}
-
-	var strBegin = len(getEchoPrefix) // GET /echo/
-	var strEnd = strBegin
-	for req[strEnd] != ' ' {
-		strEnd++
+	if isGet(&req) && bytes.HasPrefix(path, []byte("/echo")) {
+		after, _ := bytes.CutPrefix(path, []byte("/echo/"))
+		var body = make([]byte, 0, len(after))
+		body = append(body, after...)
+		respondGet(&conn, 200, &body)
+		return
 	}
-
-	var header = []byte("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ")
-	var conLen = strEnd - strBegin
-	var bSlcConLen = []byte(strconv.Itoa(conLen))
-	var str = make([]byte, 0, len(header)+len(bSlcConLen)+4+conLen)
-
-	str = append(str, header...)
-	str = append(str, bSlcConLen...)
-	str = append(str, []byte("\r\n\r\n")...)
-	for i := strBegin; i < strEnd; i++ {
-		str = append(str, req[i])
-	}
-	conn.Write(str)
+	conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
 }
